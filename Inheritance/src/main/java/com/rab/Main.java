2fe9df632ecf67266365218e5026f1116bddd039
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-06-09 23:58:11 +0200

    Inheritance

--- Inheritance/src/main/java/com/rab/Main.java
+++ Inheritance/src/main/java/com/rab/Main.java
@@ -5,7 +5,12 @@ package com.rab;
 public class Main {
     public static void main(String[] args) {
         Person person = new Person();
+        Student student = new Student();
         String value = person.toString();
+        person.setName("Herve");
+        person.setEmail("herve.irakoze@auca.ac.rw");
+        student.setName("Nsumba");
+        student.setCollege("Auca");
         System.out.println(person);
         System.out.println(value);
     }
--- Inheritance/src/main/java/com/rab/Student.java
+++ Inheritance/src/main/java/com/rab/Student.java
@@ -4,9 +4,19 @@ public class Student  extends Person{
     private String college;
     private String faculty;
 
-    public Student(String name, String email, String phoneNumber, String college, String faculty) {
-        super(name, email, phoneNumber);
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
         this.college = college;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
         this.faculty = faculty;
     }
 }
