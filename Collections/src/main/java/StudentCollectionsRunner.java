1bb9dbf274b326ea6f318fc9e15025b222509142
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-06-10 23:21:18 +0200

    Collections

---
+++ Collections/src/main/java/StudentCollectionsRunner.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class StudentCollectionsRunner {
+    public static void main(String[] args) {
+        List<Student> students = List.of(new Student(1, "Range"), new Student(100, "Herve"), new Student(99, "Nsumba"));
+        System.out.println(students);
+        ArrayList<Student> studentsAll = new ArrayList<>(students);
+        Collections.sort(studentsAll);
+        System.out.println(studentsAll);
+    }
+}
