59ae802ee6b38fb9c66414f29ac5cf8dc3f6b0d0
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-06-12 21:41:31 +0200

    Queue Interface

---
+++ Collections/src/main/java/QueueInterface/QueueRunner.java
@@ -0,0 +1,24 @@
+package QueueInterface;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class StringLengthComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String value1, String value2) {
+        return Integer.compare(value1.length(), value2.length());
+    }
+}
+public class QueueRunner {
+    public static void main(String[] args) {
+        Queue<String> queue = new PriorityQueue<>(new StringLengthComparator());
+        queue.addAll(List.of("Crocodile","Rabbit","Hen","Lion"));
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+    }
+}
