5623bf4d5ac7e19f4c0ae9534c0b1c7d4cbfe256
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-06-21 20:13:06 +0200

    Intermediate operations in functional programming

--- FunctionalProgramming/src/main/java/SumOfListRunner/SumOfListFpRunner.java
+++ FunctionalProgramming/src/main/java/SumOfListRunner/SumOfListFpRunner.java
@@ -1,7 +1,28 @@
 package SumOfListRunner;
 
+import java.util.List;
+
 public class SumOfListFpRunner {
     public static void main(String[] args) {
-        
+        List<Integer> numbers = List.of(1,6,9,35,8);
+        //numbers.stream().forEach(element -> System.out.println(element));
+        int sum = normalSum(numbers);
+        System.out.println(sum);
+
+        int sumOfEvenNumbers = getSumOfEvenNumbers(numbers);
+        System.out.println(sumOfEvenNumbers);
+    }
+
+    private static int getSumOfEvenNumbers(List<Integer> numbers) {
+        int sumOfEvenNumbers = numbers.stream().
+                filter(number -> number % 2 == 0).
+                reduce(0, (number1, number2) -> number1 + number2);
+        return sumOfEvenNumbers;
+    }
+
+    private static int normalSum(List<Integer> numbers) {
+        int sum = numbers.stream().
+                reduce(0, (number1, number2) -> number1 + number2);
+        return sum;
     }
 }
