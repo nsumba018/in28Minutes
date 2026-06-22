73fabe6bc7b82f02536718d0f19b2966a9ced5bd
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-06-22 20:06:03 +0200

    Thread Set priority adn join method

--- Threads-MultiThreading/src/main/java/com/rab/ThreadsBasicsRunner.java
+++ Threads-MultiThreading/src/main/java/com/rab/ThreadsBasicsRunner.java
@@ -18,17 +18,23 @@ class Task2 implements Runnable{
 
 }
 public class ThreadsBasicsRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //Task 1
         System.out.print("Task1 Kicked Off");
         Task1 task1 = new Task1();
+        task1.setPriority(1);
         task1.start();
+
         // Task 2
         System.out.print("Task2 Kicked Off");
         Task2 task2 = new Task2(); // when you use implements Runnable you have to create instance if Threads
         Thread task2Thread = new Thread(task2);
-        task2Thread.start();
+        task2Thread.setPriority(10);
+
+        // wait for task1 to finish
+        task1.join();
+        
         System.out.print("Task3 Kicked Off");
         for(int i = 300; i < 399; i++)
             System.out.print(i + " ");
