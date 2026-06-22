e356c7ff65292b47f842a16649df7dd81f1b92a9
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-06-22 19:51:25 +0200

    Threads Basics

--- Threads-MultiThreading/src/main/java/com/rab/ThreadsBasicsRunner.java
+++ Threads-MultiThreading/src/main/java/com/rab/ThreadsBasicsRunner.java
@@ -1,17 +1,38 @@
 package com.rab;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Task1 extends Thread{
+        // Task 1
+    public void run(){
+        for(int i = 0; i < 100; i++)
+            System.out.print(i + " ");
+        System.out.print("\nTask 1 Done\n");
+    }
+
+}
+class Task2 implements Runnable{
+    public void run(){
+        for(int i = 200; i < 299; i++)
+            System.out.print(i + " ");
+        System.out.print("\nTask 2 Done\n");
+    }
+
+}
 public class ThreadsBasicsRunner {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Task 1
+        System.out.print("Task1 Kicked Off");
+        Task1 task1 = new Task1();
+        task1.start();
+        // Task 2
+        System.out.print("Task2 Kicked Off");
+        Task2 task2 = new Task2(); // when you use implements Runnable you have to create instance if Threads
+        Thread task2Thread = new Thread(task2);
+        task2Thread.start();
+        System.out.print("Task3 Kicked Off");
+        for(int i = 300; i < 399; i++)
+            System.out.print(i + " ");
+        System.out.print("\nTask 3 Done");
+
     }
 }
